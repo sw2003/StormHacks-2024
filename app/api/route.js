ff07ae8bd9f988065a2f889fca7fe7e28151930a
Sam <samwentupp@outlook.com>
2024-10-05 12:00:43 -0700

    backend setup

---
+++ app/api/route.js
@@ -0,0 +1,7 @@
+// take the audio file 
+// use deepgram api to convert into transcript
+// return the markdown to the frontend
+
+export default function POST(req){
+    return Response.json({}) 
+}
