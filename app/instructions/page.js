4f9df89c351938172a7518d2e20d8228dd612585
Sam <samwentupp@outlook.com>
2024-10-05 13:00:06 -0700

    for james

---
+++ app/instructions/page.js
@@ -0,0 +1,18 @@
+import Image from "next/image"
+
+// Github Instructions on how to install 
+
+
+export default function Page(){
+    return (
+        <>
+            <div>Instructions</div>   
+
+            {
+                /*
+                Feel free to add more 
+                */
+            } 
+        </>
+    )
+}
