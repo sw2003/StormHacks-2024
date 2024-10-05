aa266f67deed850c8d1c653accc5b4f4f2a5dc2a
Sam <samwentupp@outlook.com>
2024-10-05 12:37:01 -0700

    add icon to record button

--- app/page.js
+++ app/page.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import {Button} from '@nextui-org/button'; 
+import { RiRecordCircleLine } from "react-icons/ri";
 
 export default function Home() {
   return (
       <div className=" max-w-[768px] mx-auto">
         <div className="p-2">
-          <div className="p-2 bg-red-500 rounded text-center cursor-pointer">
+          <div className="p-2 bg-red-500 rounded-full text-center cursor-pointer flex gap-3 items-center justify-center">
+            <RiRecordCircleLine size={20}></RiRecordCircleLine>
             Record
           </div>
         </div>      
